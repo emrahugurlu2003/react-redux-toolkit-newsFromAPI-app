a5b959cf5665a0889c633c8d472f76cb5a1d530a
emrahugurlu2003 <135663130+emrahugurlu2003@users.noreply.github.com>
2023-08-16 14:55:37 +0300

    store initial

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,1 +1,13 @@
+//!redux-toolkit içinden configureStore metodu named import edilir.
 import { configureStore } from "@reduxjs/toolkit";
+//!yazdığımız slice'lar default import şeklinde import edilir:
+import authReducer from "../features/authSlice";
+import newsReducer from "../features/newsSlice";
+
+//!İki reducer burada birleştirilerek bir store oluşturulur
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    api: newsReducer,
+  },
+});
